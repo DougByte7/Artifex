26b5d6bbbb9609e956a446e568e6255f8745f280
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2025-03-18 14:22:35 -0300

    Fix vercel build?????

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@tanstack/react-start/config"
 import tsConfigPaths from "vite-tsconfig-paths"
 import tailwindcss from "@tailwindcss/vite"
+import path from "node:path"
 
 export default defineConfig({
   vite: {
@@ -10,5 +11,10 @@ export default defineConfig({
         projects: ["./tsconfig.json"],
       }),
     ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./app"),
+      },
+    },
   },
 })
