66e6b17ebd15557fb4b9f31ef5b6db4318d6645d
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2025-03-24 19:30:01 -0300

    Add base combat system

--- app/components/game/battle-scene.tsx
+++ app/components/game/battle-scene.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import {
+  hpFormula,
   manaFormula,
-  useCharacter,
-  useEnemies,
-  useGameActions,
-  useHitInfo,
-  useTurn,
+  reactionFormula,
+  useGameStore,
+  useIsPlayerTurn,
   type Enemy,
+  type ReactionType,
 } from "./state"
 import { randomBetween } from "@/utils"
 import { nanoid } from "nanoid"
@@ -25,17 +25,37 @@ function buildEnemy(): Enemy {
 }
 
 export function BattleScene() {
-  const char = useCharacter()
-  const enemies = useEnemies()
-  const { setEnemies, attack } = useGameActions()
-  const hitInfo = useHitInfo()
-  const turn = useTurn()
+  const { startCombat, attack, enemyAttack, nextTurn } =
+    useGameStore.use.actions()
+  const char = useGameStore.use.character()
+  const enemies = useGameStore.use.enemies()
+  const hitInfo = useGameStore.use.hitInfo()
+  const turn = useGameStore.use.turn()
+  const isPlayerTurn = useIsPlayerTurn()
 
   useEffect(() => {
-    setEnemies([buildEnemy()])
+    startCombat([
+      {
+        id: nanoid(8),
+        name: "Garrik",
+        hp: 15,
+        reactions: 3,
+        reactionChance: 0.75,
+        strength: 3,
+        agility: 1,
+      },
+    ])
   }, [])
 
-  const handleAttack = (spellIndex: number) => () => attack(spellIndex, 0)
+  const handleAttack = (spellIndex: number) => () => {
+    attack(spellIndex, 0)
+    nextTurn()
+  }
+
+  const handleEnemyAttack = (type?: ReactionType) => () => {
+    enemyAttack(type)
+    nextTurn()
+  }
 
   return (
     <section className="grid gap-4">
@@ -47,51 +67,95 @@ export function BattleScene() {
       </h1>
 
       <div className="flex gap-4">
-        {enemies.map((enemy) => (
-          <div
-            key={enemy.id}
-            className="grid w-fit gap-2 rounded border border-stone-500 bg-stone-200/5 p-4"
-          >
-            <p>Name: {enemy.name}</p>
-            <p>HP: {enemy.hp}</p>
-            <p>Reactions: {enemy.reactions}</p>
-            <p>Reaction Chance: {(enemy.reactionChance * 100).toFixed(2)}%</p>
-            <p>Strength: {enemy.strength}</p>
-            <p>Agility: {enemy.agility}</p>
-            <p>
-              Power:{" "}
-              {enemy.hp + enemy.reactions + enemy.strength + enemy.agility}
-            </p>
+        {enemies.length ? (
+          enemies.map((enemy) => (
+            <div
+              key={enemy.id}
+              className="grid w-fit gap-2 rounded border border-stone-500 bg-stone-200/5 p-4"
+            >
+              <p>Name: {enemy.name}</p>
+              <p>HP: {enemy.hp}</p>
+              <p>Reactions: {enemy.reactions}</p>
+              <p>Reaction Chance: {(enemy.reactionChance * 100).toFixed(2)}%</p>
+              <p>Strength: {enemy.strength}</p>
+              <p>Agility: {enemy.agility}</p>
+              <p>
+                Power:{" "}
+                {enemy.hp + enemy.reactions + enemy.strength + enemy.agility}
+              </p>
+            </div>
+          ))
+        ) : (
+          <div className="grid w-fit gap-2 rounded border border-stone-500 bg-stone-200/5 p-4">
+            <p>Combate encerrado...</p>
           </div>
-        ))}
+        )}
 
         <div className="grid w-fit gap-2 rounded border border-stone-500 bg-stone-200/5 p-4">
-          <p>Hollow lv. 10</p>
-          <p>
-            HP: {char.hp} / {char.strength / 2}
-          </p>
-          <p>
-            Mana: {char.mana} / {manaFormula(char.will)}
-          </p>
-          <p>Reactions: {char.reactions}</p>
-          <p>Strength: {char.strength}</p>
-          <p>Agility: {char.agility}</p>
-          <p>Will: {char.will}</p>
+          {char.currentHp > 0 ? (
+            <>
+              <p>Hollow lv. 10</p>
+              <p>
+                HP: {char.currentHp} / {hpFormula(char.strength)}
+              </p>
+              <p>
+                Mana: {char.currentMana} / {manaFormula(char.will)}
+              </p>
+              <p>
+                Reactions: {char.currentReactions} /{" "}
+                {reactionFormula(char.agility)}
+              </p>
+              <p>Strength: {char.strength}</p>
+              <p>Agility: {char.agility}</p>
+              <p>Will: {char.will}</p>
+            </>
+          ) : (
+            <p>No céu tem pão?</p>
+          )}
         </div>
       </div>
 
-      <div className="flex gap-2">
-        <Button onClick={handleAttack(-1)}>Sword Attack</Button>
-        {char.spells.map((spell, i) => (
-          <Button
-            key={spell.name}
-            disabled={char.mana < spell.cost}
-            onClick={handleAttack(i)}
-          >
-            {spell.name} - {spell.cost} MP - {spell.damage} Dmg
-          </Button>
-        ))}
-      </div>
+      {char.currentHp > 0 && (
+        <div className="flex gap-2">
+          {isPlayerTurn ? (
+            <>
+              <Button onClick={handleAttack(-1)}>Sword Attack</Button>
+              {char.spells.map((spell, i) => (
+                <Button
+                  key={spell.name}
+                  disabled={char.mana < spell.cost}
+                  onClick={handleAttack(i)}
+                >
+                  {spell.name} - {spell.cost} MP - {spell.damage} Dmg
+                </Button>
+              ))}
+            </>
+          ) : (
+            <>
+              <Button onClick={handleEnemyAttack()}>Sofrer o dano</Button>
+              <Button
+                disabled={!char.currentReactions}
+                onClick={handleEnemyAttack("dodge")}
+              >
+                Desviar (Teste 6 de agilidade: Zera o dano recebido)
+              </Button>
+              <Button
+                disabled={!char.currentReactions}
+                onClick={handleEnemyAttack("block")}
+              >
+                Bloquear (Teste 4 de força: Reduz o dano pela metade)
+              </Button>
+              <Button
+                disabled={!char.currentReactions}
+                onClick={handleEnemyAttack("parry")}
+              >
+                Contra-Atacar (Teste 4 de agilidade ou força; oque for melhor:
+                causa metade da força como dano)
+              </Button>
+            </>
+          )}
+        </div>
+      )}
     </section>
   )
 }
--- app/components/game/state.ts
+++ app/components/game/state.ts
@@ -1,4 +1,4 @@
-import { create } from "zustand"
+import { create, type StoreApi, type UseBoundStore } from "zustand"
 import { immer } from "zustand/middleware/immer"
 
 import type { Spell } from "@/gameplay/scripts/Spell"
@@ -34,6 +34,8 @@ export interface Enemy {
 
 interface GameState {
   turn: number
+  turnPriority: number
+  turnOrder: string[]
   hitInfo: string
   character: Character
   enemies: Enemy[]
@@ -41,39 +43,60 @@ interface GameState {
 
 interface GameActions {
   actions: {
-    setEnemies: (enemies: Enemy[]) => void
+    startCombat: (enemies: Enemy[]) => void
     attack: (spellIndex: number, enemyIndex: number) => void
+    enemyAttack: (reactionType?: ReactionType) => void
+    nextTurn: VoidFunction
   }
 }
 
+export type ReactionType = "dodge" | "block" | "parry"
+
 export const manaFormula = (will: number) => 80 + will * 10
+export const hpFormula = (strength: number) => strength * 2
+export const reactionFormula = (agility: number) => (agility / 2) | 0
+
+const BASE_STRENGTH = 4
+const BASE_AGILITY = 2
+const BASE_WILL = 2
 
 // DO NOT EXPORT THIS | DO NOT ACCESS THE STORE DIRECTLY OUTSIDE OF THIS FILE
-const useGameStore = create<GameState & GameActions>()(
+const gameStore = create<GameState & GameActions>()(
   immer((set) => ({
-    turn: 1,
-    hitInfo: "",
     character: {
       spells: [],
-      strength: 4,
-      agility: 2,
-      will: 2,
+      strength: BASE_STRENGTH,
+      agility: BASE_AGILITY,
+      will: BASE_WILL,
       get hp() {
-        return this.strength / 2
+        return hpFormula(this.strength)
       },
       get reactions() {
-        return this.agility / 2
+        return reactionFormula(this.agility)
       },
       get mana() {
         return manaFormula(this.will)
       },
-      currentHp: 2,
-      currentReactions: 1,
-      currentMana: 100,
+      currentHp: hpFormula(BASE_STRENGTH),
+      currentReactions: reactionFormula(BASE_AGILITY),
+      currentMana: manaFormula(BASE_WILL),
     },
     enemies: [],
+    turn: 1,
+    turnPriority: 0,
+    turnOrder: [],
+    hitInfo: "",
     actions: {
-      setEnemies: (enemies) => set({ enemies }),
+      startCombat: (enemies) =>
+        set((prev) => ({
+          turn: 1,
+          turnPriority: 0,
+          enemies,
+          turnOrder: [...enemies, prev.character]
+            .map((e) => ({ ...e, initiative: diceRoll() + e.agility }))
+            .sort((a, b) => a.initiative - b.initiative)
+            .map((e) => ("id" in e ? e.id : "player")),
+        })),
       attack: (spellIndex, enemyIndex) =>
         set((prev) => {
           const char = prev.character
@@ -90,14 +113,15 @@ const useGameStore = create<GameState & GameActions>()(
 
           const hasReaction = enemy.reactions > 0
           const willUseReaction =
-            doesHit && hasReaction
-              ? Math.random() < enemy.reactionChance ||
-                (isCritical && Math.random() < enemy.reactionChance)
-              : false
-          const dodged = willUseReaction
-            ? diceRoll() + (isSword ? enemy.strength : enemy.agility) >
+            doesHit &&
+            hasReaction &&
+            (Math.random() < enemy.reactionChance ||
+              (isCritical && Math.random() < enemy.reactionChance))
+
+          const dodged =
+            willUseReaction &&
+            diceRoll() + (isSword ? enemy.strength : enemy.agility) >
               (isCritical ? 6 : 4)
-            : false
 
           const damage =
             dodged || !doesHit
@@ -116,9 +140,8 @@ const useGameStore = create<GameState & GameActions>()(
           )
 
           enemy.hp -= damage
-          enemy.reactions = willUseReaction
-            ? enemy.reactions - 1
-            : enemy.reactions
+          enemy.reactions = enemy.reactions - +willUseReaction
+          if (enemy.hp <= 0) prev.enemies.splice(enemyIndex, 1)
 
           prev.hitInfo = dodged
             ? "Dodged"
@@ -129,35 +152,83 @@ const useGameStore = create<GameState & GameActions>()(
                     : ""
                 }Attack <span class="text-blue-500">${spellIndex === -1 ? "Sword" : char.spells[spellIndex].name}</span> hit for <span class="text-red-500">${damage} damage</span>`
               : "Missed"
+        }),
+      enemyAttack: (reactionType) =>
+        set((prev) => {
+          const enemy = prev.enemies.find(
+            (e) => e.id === prev.turnOrder[prev.turnPriority],
+          )
+          if (!enemy) return
 
+          const player = prev.character
+
+          const attackChance =
+            diceRoll() + Math.max(enemy.strength, enemy.agility)
+
+          const doesHit = attackChance > 4
+          const isCritical = attackChance > 6
+
+          const dodged =
+            reactionType === "dodge" && diceRoll() + player.agility > 6
+          const block =
+            reactionType === "block" &&
+            diceRoll() + Math.max(player.strength) > 4
+          const parried =
+            reactionType === "parry" &&
+            diceRoll() + Math.max(player.strength, player.agility) > 6
+
+          const damage =
+            doesHit && !dodged
+              ? Math.max(
+                  (isCritical ? enemy.strength * 2 : enemy.strength) /
+                    (block ? 2 : 1),
+                  0,
+                )
+              : 0
+
+          if (parried) enemy.hp -= player.strength / 2
+
+          player.currentHp -= damage
+          player.currentReactions -= reactionType ? 1 : 0
+          prev.hitInfo =
+            doesHit && !dodged
+              ? `Enemy ${isCritical ? "critical " : " "}hit for <span class="text-red-500">${damage} damage</span> ${block ? "(Blocked)" : ""} ${parried ? "(Parried)" : ""}`
+              : dodged
+                ? "Dodged"
+                : `Missed ${parried ? "(Parried)" : ""}`
+        }),
+      nextTurn: () =>
+        set((prev) => {
           prev.turn++
+          prev.turnPriority = (prev.turnPriority + 1) % prev.turnOrder.length
         }),
     },
   })),
 )
 
-// Actions never change so it's safe to subscribe to them all
-export function useGameActions() {
-  const actions = useGameStore((state) => state.actions)
-  return actions
+type WithSelectors<S> = S extends { getState: () => infer T }
+  ? S & { use: { [K in keyof T]: () => T[K] } }
+  : never
+
+const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(
+  _store: S,
+) => {
+  const store = _store as WithSelectors<typeof _store>
+  store.use = {}
+  for (const k of Object.keys(store.getState())) {
+    // @ts-expect-error TS is being a little bitch
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    store.use[k as any] = () => store((s) => s[k as keyof typeof s])
+  }
+
+  return store
 }
 
-export function useCharacter() {
-  const character = useGameStore((state) => state.character)
-  return character
-}
+export const useGameStore = createSelectors(gameStore)
 
-export function useEnemies() {
-  const enemies = useGameStore((state) => state.enemies)
-  return enemies
-}
-
-export function useTurn() {
-  const turn = useGameStore((state) => state.turn)
-  return turn
-}
-
-export function useHitInfo() {
-  const hitInfo = useGameStore((state) => state.hitInfo)
-  return hitInfo
+export function useIsPlayerTurn() {
+  const turnOrder = useGameStore.use.turnOrder()
+  const turnPriority = useGameStore.use.turnPriority()
+  const isPlayerTurn = turnOrder[turnPriority] === "player"
+  return isPlayerTurn
 }
